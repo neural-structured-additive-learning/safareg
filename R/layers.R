892ba997046a2c049f5ef00ac63502b86b40bfdf
davidruegamer <david.ruegamer@stat.uni-muenchen.de>
2021-09-29 23:11:23 +0200

    add interaction and factorization machines

--- R/layers.R
+++ R/layers.R
@@ -1,18 +1,20 @@
 # tp and ttp have different positioning internally (b,a instead of a,b)
 # as this results in a sparser design and penalty matrix
-tp_layer = function(a, b, pen=NULL, name=NULL, units = 1) {
-  x <- tf_row_tensor(b, a) %>% 
-    layer_dense(units = units, activation = "linear", 
-                name = name, use_bias = FALSE, 
-                kernel_regularizer = pen)
+tp_layer = function(a, b, pen=NULL, name=NULL, units = 1, with_layer = TRUE) {
+  x <- tf_row_tensor(b, a)
+  if(with_layer) x <- x %>% layer_dense(units = units, activation = "linear", 
+                                        name = name, use_bias = FALSE, 
+                                        kernel_regularizer = pen)
   return(x)
 }
 
-ttp_layer = function(a, b, c, pen=NULL, name=NULL, units = 1) {
-  x <- tf_row_tensor(tf_row_tensor(b, c), a) %>% 
-    layer_dense(units = units, activation = "linear", 
-                name = name, use_bias = FALSE, 
-                kernel_regularizer = pen)
+ttp_layer = function(a, b, c, pen=NULL, name=NULL, units = 1, with_layer = TRUE) {
+  
+  x <- tf_row_tensor(tf_row_tensor(b, c), a) 
+  if(with_layer) x <- x %>% layer_dense(units = units, activation = "linear", 
+                                        name = name, use_bias = FALSE, 
+                                        kernel_regularizer = pen)
+  
   return(x)
 }
 
@@ -24,7 +26,7 @@ pen_vc <- function(P, strength, nlev)
   
 }
 
-vc_block <- function(ncolNum, levFac, penalty = NULL, name = NULL, units = 1){
+vc_block <- function(ncolNum, levFac, penalty = NULL, name = NULL, units = 1, with_layer = TRUE){
   
   if(!is.null(penalty))
     penalty = pen_vc(penalty, strength=1, nlev=levFac)
@@ -36,13 +38,13 @@ vc_block <- function(ncolNum, levFac, penalty = NULL, name = NULL, units = 1){
       (tf_stride_cols(x, ncolNum+1)[,1]), 
       dtype="int32"), 
       depth=levFac)
-    return(tp_layer(a, b, pen=penalty, name=name, units=units))
+    return(tp_layer(a, b, pen=penalty, name=name, units=units, with_layer=with_layer))
     
   }
   return(ret_fun)
 }
 
-vvc_block <- function(ncolNum, levFac1, levFac2, penalty = NULL, name = NULL, units = 1){
+vvc_block <- function(ncolNum, levFac1, levFac2, penalty = NULL, name = NULL, units = 1, with_layer = TRUE){
   
   if(!is.null(penalty))
     penalty = pen_vc(penalty, strength=1, nlev=levFac1*levFac2)
@@ -51,7 +53,8 @@ vvc_block <- function(ncolNum, levFac1, levFac2, penalty = NULL, name = NULL, un
                                    tf$one_hot(tf$cast(x[,as.integer((ncolNum+1))], dtype="int32"), 
                                               depth=levFac1),
                                    tf$one_hot(tf$cast(x[,as.integer((ncolNum+2))], dtype="int32"), 
-                                              depth=levFac2), pen=penalty, name=name, units=units)
+                                              depth=levFac2), 
+                                   pen=penalty, name=name, units=units, with_layer=with_layer)
   
   return(ret_fun)
 }
@@ -86,4 +89,75 @@ layer_random_effect <- function(freq, df)
                  kernel_regularizer = regularizer_l2(l = lambda/sum(freq)))
   )
   
+}
+
+factorization_machine <- function(embedding_dim,
+                                  xlev,
+                                  ylev,
+                                  zlev) {
+  
+  dotprod <- function(a,b) tf$math$reduce_sum(tf$multiply(a,b), 
+                                              axis=2L)
+  
+  dot_fun <- keras_model_custom(name = "factorization_machine", function(self) {
+    self$x_embedding <-
+      layer_embedding(
+        input_dim = xlev + 1,
+        output_dim = embedding_dim,
+        embeddings_initializer = initializer_random_uniform(minval = 0, maxval = 0.05),
+        name = "first_level_embedding"
+      )
+    self$y_embedding <-
+      layer_embedding(
+        input_dim = ylev + 1,
+        output_dim = embedding_dim,
+        embeddings_initializer = initializer_random_uniform(minval = 0, maxval = 0.05),
+        name = "second_level_embedding"
+      )
+    
+    if(is.null(zlev)){
+      self$dot <-
+        layer_lambda(
+          f = function(x) {
+            dotprod(x[[1]], x[[2]])
+          }
+        )
+      return(function(x, mask = NULL, training = FALSE) {
+        y <- tf_stride_cols(x, 2L)
+        x <- tf_stride_cols(x, 1L)
+        x_embedding <- self$x_embedding(x)
+        y_embedding <- self$y_embedding(y)
+        self$dot(list(x_embedding, 
+                      y_embedding))
+      })
+    }else{
+      self$z_embedding <-
+        layer_embedding(
+          input_dim = zlev + 1,
+          output_dim = embedding_dim,
+          embeddings_initializer = initializer_random_uniform(minval = 0, maxval = 0.05),
+          name = "third_level_embedding"
+        )
+      self$dot <-
+        layer_lambda(
+          f = function(x) {
+            dotprod(dotprod(x[[1]], x[[2]]), x[[3]])
+          }
+  )
+  return(function(x, mask = NULL, training = FALSE) {
+    y <- tf_stride_cols(x, 2L)
+    z <- tf_stride_cols(z, 3L)
+    x <- tf_stride_cols(x, 1L)
+    x_embedding <- self$x_embedding(x)
+    y_embedding <- self$y_embedding(y)
+    z_embedding <- self$z_embedding(activities)
+    self$dot(list(x_embedding, 
+                  y_embedding, 
+                  z_embedding))
+  })
+    }
+  })
+
+return(dot_fun)
+
 }
--- R/main.R
+++ R/main.R
@@ -1,23 +1,96 @@
-fac_processor <- function(term, data, output_dim, param_nr){
+onehotfac <- function(x, lev){ 
+  
+  tf$squeeze(tf$one_hot(tf$cast(x, dtype="int32"), 
+                        depth = lev), 
+             axis=1L)
+  
+}
+  
+onehotia <- function(x, xlev, ylev){
+  
+  y = tf_stride_cols(x, 2L)
+  x = tf_stride_cols(x, 1L)
+  xoh = onehotfac(x, xlev)
+  yoh = onehotfac(y, ylev)
+  return(tf_row_tensor(xoh, yoh))
+  
+}
+
+#' Processor for factor terms with many levels
+#' 
+#' @export
+#' 
+fac_processor <- function(term, data, output_dim, param_nr, controls = NULL){
   # factor_layer
-  list(
-    data_trafo = function() as.integer(data[extractvar(term)]),
-    predict_trafo = function(newdata) as.integer(newdata[extractvar(term)]),
-    input_dim = as.integer(extractlen(term, data)),
+  la <- extractval(term, "la", null_for_missing = TRUE)
+  kr <- NULL
+  if(!is.null(la))
+    kr <- tf$keras$regularizers$l2(l = la)
+  
+  if(controls$with_layer){
     layer = function(x, ...)
-      return(tf$one_hot(tf$cast(x, dtype="int32"), 
-                        depth = nlevels(data[[extractvar(term)]])) %>% 
-               tf$keras$layers$Dense(
-                 units = output_dim,
-                 kernel_regularizer = tf$keras$regularizers$l2(l = extractval(term, "la")),
-                 name = makelayername(term, 
-                                      param_nr),
-                 ...
-               )),
+      return(tf$keras$layers$Dense(
+        units = output_dim,
+        kernel_regularizer = kr,
+        name = makelayername(term, 
+                             param_nr),
+        ...
+      )(onehotfac(x, nlevels(data[[extractvar(term)]]))))
+  }else{
+    layer = function(x) onehotfac(x, nlevels(data[[extractvar(term)]]))
+  }
+    
+  list(
+    data_trafo = function() lapply(data[extractvar(term)], as.integer),
+    predict_trafo = function(newdata) lapply(newdata[extractvar(term)], as.integer),
+    input_dim = as.integer(extractlen(term, data)),
+    layer = layer,
     coef = function(weights) as.matrix(weights)
   )
 }
 
+#' Processor for interactions of factor terms with many levels
+#' 
+#' @export
+#' 
+interaction_processor <- function(term, data, output_dim, param_nr, controls = NULL){
+  # factor_layer
+  la <- extractval(term, "la", null_for_missing = TRUE)
+  kr <- NULL
+  if(!is.null(la))
+    kr <- tf$keras$regularizers$l2(l = la)
+  trms <- extractvar(term)
+  xlev <- nlevels(data[[trms[1]]])
+  ylev <- nlevels(data[[trms[2]]])
+  
+  if(controls$with_layer){
+    layer = function(x, ...)
+      return(tf$keras$layers$Dense(
+        units = output_dim,
+        kernel_regularizer = kr,
+        name = makelayername(term, 
+                             param_nr),
+        ...
+      )(onehotia(x, xlev = xlev, 
+                 ylev = ylev)))
+  }else{
+    layer = function(x) onehotia(x, xlev = xlev, 
+                                 ylev = ylev)
+  }
+  
+  list(
+    data_trafo = function() lapply(data[extractvar(term)], as.integer),
+    predict_trafo = function(newdata) lapply(newdata[extractvar(term)], as.integer),
+    input_dim = as.integer(extractlen(term, data)),
+    layer = layer,
+    coef = function(weights) as.matrix(weights)
+  )
+}
+
+#' Processor for varying coefficient terms with many factor levels
+#' 
+#' @export
+#' 
 vc_processor <- function(term, data, output_dim, param_nr, controls){
   # vc (old: vc, vcc)
   vars <- extractvar(term)
@@ -43,10 +116,12 @@ vc_processor <- function(term, data, output_dim, param_nr, controls){
   
   if(length(nlev)==1){
     layer <- vc_block(ncolNum, nlev, penalty = controls$sp_scale(data) * P, 
-                      name = makelayername(term, param_nr), units = units)
+                      name = makelayername(term, param_nr), units = units,
+                      with_layer = controls$with_layer)
   }else if(length(nlev)==2){
     layer <- vvc_block(ncolNum, nlev[1], nlev[2], penalty = controls$sp_scale(data) * P, 
-                       name = makelayername(term, param_nr), units = units)
+                       name = makelayername(term, param_nr), units = units,
+                       with_layer = controls$with_layer)
   }else{
     stop("vc terms with more than 2 factors currently not supported.")
   }
@@ -63,3 +138,28 @@ vc_processor <- function(term, data, output_dim, param_nr, controls){
   )
 }
 
+fm_processor <- function(term, data, output_dim, param_nr, controls = NULL){
+  # factor_layer
+  dim <- extractval(term, "dim")
+  if(is.null(dim)) dim <- 10
+  trms <- extractvar(term)
+  xlev <- nlevels(data[[trms[1]]])
+  ylev <- nlevels(data[[trms[2]]])
+  zlev <- NULL
+  if(length(trms)==3) zlev <- nlevels(data[[trms[3]]])
+  fm <- factorization_machine(
+    dim,
+    xlev,
+    ylev,
+    zlev)
+  
+  layer = function(x, ...) fm(x)
+  
+  list(
+    data_trafo = function() lapply(data[extractvar(term)], as.integer),
+    predict_trafo = function(newdata) lapply(newdata[extractvar(term)], as.integer),
+    input_dim = as.integer(extractlen(term, data)),
+    layer = layer,
+    coef = function(weights) as.matrix(weights)
+  )
+}
